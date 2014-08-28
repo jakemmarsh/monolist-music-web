bc8be97281d4813046cc2765d12aaac257c638bc
Jake Marsh <jakemmarsh@gmail.com>
2014-08-28 14:49:54 -0400

    Finish Bandcamp streaming endpoint logic

--- api/routes/search.js
+++ api/routes/search.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = function(req, res) {
+
+  // Search all resources. Bandcamp, Spotify, Soundcloud, etc.
+
   res.status(200).send('Searching for: ' + req.params.query);
+
 };
