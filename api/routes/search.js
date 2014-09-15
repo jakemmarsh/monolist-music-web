24004a2dd303c9a02cb20c4632ed56c1be1b37a4
Jake Marsh <jakemmarsh@gmail.com>
2014-09-14 21:31:38 -0400

    Begin logic for user-defined sources to search

--- api/routes/search.js
+++ api/routes/search.js
@@ -11,16 +11,37 @@ var youtube    = require(path.join(__dirname, 'sources/youtube'));
 
 module.exports = function(req, res) {
 
+  var searchSources = [];
   var searchResults;
+  var sources;
 
-  // Search all resources. Bandcamp, Spotify, Soundcloud, etc.
-  Q.all([
-    bandcamp.search(req.params.query),
-    soundcloud.search(req.params.query),
-    spotify.search(req.params.query),
-    youtube.search(req.params.query)
-  ]).spread(function(bandcampResults, soundcloudResults, spotifyResults, youtubeResults) {
-    searchResults = bandcampResults.concat(soundcloudResults, spotifyResults, youtubeResults);
+  if ( req.params.sources ) {
+    sources = req.params.sources.split(',');
+    sources.forEach(function(searchSource) {
+      if ( searchSource.toLowerCase() === 'bandcamp' ) {
+        searchSources.push(bandcamp.search(req.params.query));
+      } else if ( searchSource.toLowerCase() === 'soundcloud' ) {
+        searchSources.push(soundcloud.search(req.params.query));
+      } else if ( searchSource.toLowerCase() === 'spotify' ) {
+        searchSources.push(spotify.search(req.params.query));
+      } else if ( searchSource.toLowerCase() === 'youtube' ) {
+        searchSources.push(youtube.search(req.params.query));
+      }
+    });
+  } else {
+    searchSources = [
+      bandcamp.search(req.params.query),
+      soundcloud.search(req.params.query),
+      spotify.search(req.params.query),
+      youtube.search(req.params.query)
+    ];
+  }
+
+  // Search all resources
+  Q.all(searchSources).then(function(results) {
+    results.forEach(function(result) {
+      searchResults.push(result);
+    });
     res.status(200).json(searchResults);
   }, function(error) {
     res.status(500).send(error);
