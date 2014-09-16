0535f46d369428b22375e9820aab2c0f33a4cf22
Jake Marsh <jakemmarsh@gmail.com>
2014-09-16 15:14:03 -0400

    Finish Spotify search

--- api/routes/sources/spotify.js
+++ api/routes/sources/spotify.js
@@ -1,17 +1,52 @@
 'use strict';
 
-var Q = require('q');
+var Q       = require('q');
+var request = require('request');
 
 /* ====================================================== */
 
-exports.search = function(query) {
+exports.search = function(query, limit) {
 
   var mainDeferred = Q.defer();
 
   var getSearchResults = function(searchQuery) {
     var deferred = Q.defer();
+    var searchUrl = 'https://api.spotify.com/v1/search?q=';
+    var searchResults = [];
+    var trackResult;
 
-    deferred.resolve([]);
+    searchQuery = searchQuery.replace('%20', '+').replace(' ', '+');
+
+    searchUrl += searchQuery;
+    searchUrl += '&type=track';
+    searchUrl += '&limit=' + limit;
+
+    request(searchUrl, function(error, response, body){
+      if ( error ) {
+        deferred.reject(error);
+      }
+
+      // convert from string to JSON
+      body = JSON.parse(body);
+
+      if ( body.tracks ) {
+        body.tracks.items.forEach(function(item) {
+          trackResult = {
+            source: 'spotify',
+            title: item.name,
+            album: item.album ? item.album.name : null,
+            artist: item.artists ? item.artists[0].name : null,
+            image: item.album ? item.album.images[0].url : null,
+            id: item.id,
+            url: item.preview_url
+          };
+
+          searchResults.push(trackResult);
+        });
+      }
+
+      deferred.resolve(searchResults);
+    });
 
     return deferred.promise;
   };
