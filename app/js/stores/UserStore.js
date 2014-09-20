1597638395045052c1446ebc7970d7ccb95dd5bc
Jake Marsh <jakemmarsh@gmail.com>
2014-09-20 17:01:35 -0400

    add dispatch token registration

--- app/js/stores/UserStore.js
+++ app/js/stores/UserStore.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var StoreUtils = require('./utils/StoreUtils');
+var AppDispatcher = require('./dispatcher/AppDispatcher');
+var StoreUtils    = require('./utils/StoreUtils');
 
 // TODO: Logic might be different since we don't have multiple users at a time?
 var _users = {};
@@ -9,6 +10,19 @@ var UserStore = StoreUtils.createStore({
 
   get: function(login) {
     return _users[login];
+  }
+
+});
+
+UserStore.dispatchToken = AppDispatcher.register(function(payload) {
+
+  var action   = payload.action;
+  var response = action.response;
+
+  if ( response ) {
+    // TODO: what to do with response?
+    StoreUtils.mergeIntoBag(_users, response);
+    UserStore.emitChange();
   }
 
 });
