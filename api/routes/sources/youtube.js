45963cf4205ee146f814db08ae2186494a2bb43b
Jake Marsh <jakemmarsh@gmail.com>
2014-09-27 20:20:56 -0400

    Add duration to YouTube search results, convert audio to .wav

--- api/routes/sources/youtube.js
+++ api/routes/sources/youtube.js
@@ -10,9 +10,92 @@ var config        = require(path.join(__dirname, '../../../config'));
 
 /* ====================================================== */
 
+/*
+ * Convert YouTube's duration format to seconds
+ */
+function parseYouTubeDuration(duration) {
+    var a = duration.match(/\d+/g);
+
+    if (duration.indexOf('M') >= 0 && duration.indexOf('H') === -1 && duration.indexOf('S') === -1) {
+        a = [0, a[0], 0];
+    }
+
+    if (duration.indexOf('H') >= 0 && duration.indexOf('M') === -1) {
+        a = [a[0], 0, a[1]];
+    }
+    if (duration.indexOf('H') >= 0 && duration.indexOf('M') === -1 && duration.indexOf('S') === -1) {
+        a = [a[0], 0, 0];
+    }
+
+    duration = 0;
+
+    if (a.length === 3) {
+        duration = duration + parseInt(a[0]) * 3600;
+        duration = duration + parseInt(a[1]) * 60;
+        duration = duration + parseInt(a[2]);
+    }
+
+    if (a.length === 2) {
+        duration = duration + parseInt(a[0]) * 60;
+        duration = duration + parseInt(a[1]);
+    }
+
+    if (a.length === 1) {
+        duration = duration + parseInt(a[0]);
+    }
+    return duration;
+}
+
+/* ====================================================== */
+
 exports.search = function(query, limit) {
 
   var mainDeferred = Q.defer();
+
+  var getVideoDuration = function(infoUrl) {
+    var deferred = Q.defer();
+    var duration;
+
+    request(infoUrl, function(err, response, body) {
+      if ( err ) {
+        deferred.reject(err);
+      } else {
+        body = JSON.parse(body);
+
+        duration = body.items ? parseYouTubeDuration(body.items[0].contentDetails.duration) : null;
+        deferred.resolve(duration);
+      }
+    });
+
+    return deferred.promise;
+  };
+
+  var addVideoDurations = function(videos) {
+    var deferred = Q.defer();
+    var infoUrl = 'https://www.googleapis.com/youtube/v3/videos?';
+    var infoParameters = {
+      part: 'contentDetails',
+      key: config.youtube.key
+    };
+    var promises = [];
+
+    _.each(videos, function(videoObject) {
+      infoParameters.id = videoObject.sourceParam;
+
+      promises.push(getVideoDuration(infoUrl + qs.stringify(infoParameters)));
+    });
+
+    Q.all(promises).then(function(durations) {
+      _.each(durations, function(duration, index) {
+        videos[index].duration = duration;
+      });
+      deferred.resolve(videos);
+    }, function(err) {
+      deferred.reject(err);
+    });
+
+    return deferred.promise;
+  };
 
   var getSearchResults = function(searchQuery) {
     var deferred = Q.defer();
@@ -32,7 +115,6 @@ exports.search = function(query, limit) {
       if ( err ) {
         deferred.reject(err);
       } else {
-        // convert from string to JSON
         body = JSON.parse(body);
 
         // process each search result
@@ -45,7 +127,7 @@ exports.search = function(query, limit) {
           };
         });
 
-        deferred.resolve(searchResults);
+        deferred.resolve(addVideoDurations(searchResults));
       }
     });
 
@@ -69,7 +151,9 @@ exports.stream = function(req, res) {
   var requestUrl = 'http://youtube.com/watch?v=' + req.params.videoId;
 
   try {
-    youtubeStream(requestUrl).pipe(res);
+    youtubeStream(requestUrl, {
+      audioFormat: 'wav'
+    }).pipe(res);
   } catch(exception) {
     res.status(500).send(exception);
   }
