3331ee1084dc4ac845b991145ff119e73933f265
Jake Marsh <jakemmarsh@gmail.com>
2014-10-01 18:54:11 -0400

    move avatar styling into separate file, add optional 'size' prop to component

--- app/js/components/Avatar.js
+++ app/js/components/Avatar.js
@@ -7,9 +7,18 @@ var React = require('react/addons');
 
 var Avatar = React.createClass({
 
+  propTypes: {
+    size: React.PropTypes.number
+  },
+
   render: function() {
+    var styles = {
+      'height': this.props.size,
+      'width': this.props.size
+    };
+
     return (
-      <div className="avatar" />
+      <div className="avatar" style={styles} />
     );
   }
 
