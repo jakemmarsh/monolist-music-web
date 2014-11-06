4671601ccf8f5ff602b2233180b5fb7b4b931d84
Jake Marsh <jakemmarsh@gmail.com>
2014-11-05 21:40:20 -0500

    Set up unit testing framework

--- gulp/config.js
+++ gulp/config.js
@@ -1,18 +1,27 @@
 'use strict';
 
 module.exports = {
+
   'scripts': {
     'src': './app/js/**/*.js',
     'dest': './build/js/'
   },
+
   'images': {
     'src': './app/images/**/*.{jpeg,jpg,png}',
     'dest': './build/images/'
   },
+
   'styles': {
     'src': './app/styles/**/*.scss',
     'dest': './build/css/'
   },
+
   'sourceDir': './app/',
-  'buildDir': './build/'
+  'buildDir': './build/',
+
+  'test': {
+    'karma': 'test/karma.conf.js'
+  }
+
 };
