56a542c3502f31fa32d47772fbe09cde059c045e
Jake Marsh <jakemmarsh@gmail.com>
2014-11-21 17:12:42 -0500

    fix authenticate method

--- api/passport.js
+++ api/passport.js
@@ -8,24 +8,21 @@ var models        = require('./models');
 
 module.exports = function() {
 
-  passport.use(new LocalStrategy(
-    function(username, password, done) {
-      models.User.find({ username: username }).then(function(retrievedUser) {
-        retrievedUser.verifyPassword(password, function(err, result) {
-          if ( err || !result ) {
-            return done(null, false, { message: 'Incorrect password.' } );
-          } else {
-            return done(null, retrievedUser);
-          }
-        });
-      }).catch(function(err) {
-        return done(null, false, { message: err });
+  passport.use(new LocalStrategy(function(username, password, done) {
+    models.User.find({ username: username }).then(function(retrievedUser) {
+      retrievedUser.verifyPassword(password, function(err, result) {
+        if ( err || !result ) {
+          return done({ error: 'Incorrect password.' }, false);
+        } else {
+          return done(null, retrievedUser);
+        }
       });
-    }
-  ));
+    }).catch(function(err) {
+      return done({ error: err }, false);
+    });
+  }));
 
   passport.serializeUser(function(user, done) {
-    console.log('seralize:', user);
     done(null, user.username);
   });
 
