05ccf6e60073f714b0e0fa84f2ddd709fcfcb684
Jake Marsh <jakemmarsh@gmail.com>
2014-12-17 21:34:38 -0500

    Redesign profile page, add border-radius and borders to all images

---
+++ app/js/components/ProfileSidebar.js
@@ -0,0 +1,90 @@
+/**
+ * @jsx React.DOM
+ */
+'use strict';
+
+var React  = require('react/addons');
+var _      = require('lodash');
+var cx     = React.addons.classSet;
+
+var Avatar = require('./Avatar');
+
+var ProfileSidebar = React.createClass({
+
+  propTypes: {
+    currentUser: React.PropTypes.object.isRequired,
+    user: React.PropTypes.object.isRequired
+  },
+
+  getDefaultProps: function() {
+    return {
+      user: {}
+    };
+  },
+
+  getInitialState: function() {
+    return {
+      currentUserDoesFollow: !!_.where(this.props.user.subscriptions, { subscriberId: this.props.currentUser.id }).length
+    };
+  },
+
+  toggleFollowUser: function() {
+    this.setState({ currentUserDoesFollow: !this.state.currentUserDoesFollow });
+    // TODO: make database call
+  },
+
+  renderFollowButton: function() {
+    var element = null;
+    var buttonText = this.state.currentUserDoesFollow ? 'Unfollow' : 'Follow';
+    var classes = cx({
+      'action-button': true,
+      'inactive': this.state.currentUserDoesFollow
+    });
+
+    if ( true ) {
+      element = (
+        <div className={classes} onClick={this.toggleFollowUser}>
+          {buttonText}
+        </div>
+      );
+    }
+
+    return element;
+  },
+
+  render: function() {
+    return (
+      <div className="profile-sidebar">
+
+        <h4 className="username">
+          {this.props.user.username}
+          <i className="fa fa-user"></i>
+        </h4>
+
+        <div className="action-buttons-container">
+          {this.renderFollowButton()}
+        </div>
+
+        <Avatar user={this.props.user} includeLink={false} />
+
+        <div className="stats-container">
+          <div className="playlist-count-container">
+            <i className="fa fa-list"></i> {this.props.user.playlistPlays ? this.props.user.playlistPlays.length : 0}
+          </div>
+          <div className="collaboration-count-container">
+            <i className="fa fa-users"></i> {this.props.user.collaborations ? this.props.user.collaborations.length : 0}
+          </div>
+          <div className="like-count-container">
+            <i className="fa fa-heart"></i> {this.props.user.trackLikes ? this.props.user.trackLikes.length : 0}
+          </div>
+        </div>
+
+        <div className="shadow" />
+
+      </div>
+    );
+  }
+
+});
+
+module.exports = React.createFactory(ProfileSidebar);
--- app/js/pages/ProfilePage.js
+++ app/js/pages/ProfilePage.js
@@ -12,13 +12,15 @@ var GlobalActions       = require('../actions/GlobalActions');
 var ViewingProfileStore = require('../stores/ViewingProfileStore');
 var Avatar              = require('../components/Avatar');
 var PlaylistList        = require('../components/PlaylistList');
+var StarredList         = require('../components/StarredList');
+var ProfileSidebar      = require('../components/ProfileSidebar');
 
 var ProfilePage = React.createClass({
 
   mixins: [Reflux.ListenerMixin],
 
   propTypes: {
-    playlist: React.PropTypes.object
+    currentUser: React.PropTypes.object.isRequired
   },
 
   getInitialState: function() {
@@ -53,14 +55,30 @@ var ProfilePage = React.createClass({
       );
     } else {
       element = (
-        <h3 className="no-playlists">This user has not created any public playlists yet!</h3>
+        <h4 className="hard nudge--bottom">This user has not created any public playlists yet!</h4>
       );
     }
 
     return element;
   },
 
-  renderUserLikes: function() {
+  renderUserCollaborations: function() {
+    var element = null;
+
+    if ( !_.isEmpty(this.state.user.collaborations) ) {
+      element = (
+        <PlaylistList playlists={this.state.user.collaborations} />
+      );
+    } else {
+      element = (
+        <h4 className="hard nudge--bottom">This user has not collaborated on any public playlists yet!</h4>
+      );
+    }
+
+    return element;
+  },
+
+  renderUserPlaylistLikes: function() {
     var element = null;
 
     if ( !_.isEmpty(this.state.user.likes) ) {
@@ -69,7 +87,23 @@ var ProfilePage = React.createClass({
       );
     } else {
       element = (
-        <h4 className="no-liked-playlists">This user has not liked any playlists yet!</h4>
+        <h4 className="hard nudge--bottom">This user has not liked any playlists yet!</h4>
+      );
+    }
+
+    return element;
+  },
+
+  renderUserStars: function() {
+    var element = null;
+
+    if ( !_.isEmpty(this.state.user.likes) ) {
+      element = (
+        <StarredList tracks={this.state.user.stars} />
+      );
+    } else {
+      element = (
+        <h4 className="hard nudge--bottom">This user has not starred any tracks yet!</h4>
       );
     }
 
@@ -78,42 +112,57 @@ var ProfilePage = React.createClass({
 
   render: function() {
     return (
-      <section className="content profile">
+      <div>
 
-        <DocumentTitle title={this.state.user.username} />
+        <section className="content profile">
 
-        <div className="profile-header-container">
-          <div className="avatar-container">
-            <Avatar user={this.state.user} />
-          </div>
-          <div className="name-container">
-            <h3 className="name">{this.state.user.username}</h3>
-          </div>
-          <div className="buttons-container"></div>
-        </div>
+          <DocumentTitle title={this.state.user.username} />
 
-        <div className="content-container">
-          <div className="playlists-container">
-            <div className="title-container">
-              <div className="icon-container">
-                <i className="fa fa-list"></i>
+          <div className="content-container">
+
+            <div className="playlists-container">
+              <div className="title-container">
+                <div className="icon-container">
+                  <i className="fa fa-list"></i>
+                </div>
+                <h5 className="title">Playlists</h5>
               </div>
-              <h5 className="title">Playlists by {this.state.user.username}</h5>
-            </div>
-            {this.renderUserPlaylists()}
-          </div>
-          <div className="tracks-container">
-            <div className="title-container">
-              <div className="icon-container">
-                <i className="fa fa-heart"></i>
-              </div>
-              <h5 className="title">Liked</h5>
-            </div>
-            {this.renderUserLikes()}
-          </div>
-        </div>
+              {this.renderUserPlaylists()}
 
-      </section>
+              <div className="title-container">
+                <div className="icon-container">
+                  <i className="fa fa-list"></i>
+                </div>
+                <h5 className="title">Collaborations</h5>
+              </div>
+              {this.renderUserCollaborations()}
+
+              <div className="title-container">
+                <div className="icon-container">
+                  <i className="fa fa-list"></i>
+                </div>
+                <h5 className="title">Liked</h5>
+              </div>
+              {this.renderUserPlaylistLikes()}
+            </div>
+
+            <div className="stars-container">
+              <div className="title-container">
+                <div className="icon-container">
+                  <i className="fa fa-star"></i>
+                </div>
+                <h5 className="title">Starred</h5>
+              </div>
+              {this.renderUserStars()}
+            </div>
+          </div>
+
+        </section>
+
+        <nav className="sidebar right">
+          <ProfileSidebar currentUser={this.props.currentUser} user={this.state.user} />
+        </nav>
+      </div>
     );
   }
 
