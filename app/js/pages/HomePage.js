0f139c1e52b1c52d871b0a8304dd626970228366
Jake Marsh <jakemmarsh@gmail.com>
2014-12-24 13:34:58 -0500

    Finish front page design

--- app/js/pages/HomePage.js
+++ app/js/pages/HomePage.js
@@ -52,12 +52,7 @@ var HomePage = React.createClass({
         <div ref="hero" className="hero">
           <div className="hero-container wrapper">
             <h1 className="flush--top nudge-quarter--bottom">Build playlists with your friends</h1>
-            <h3 className="flush--top light">Easily add songs from multiple sources</h3>
-            <ul className="source-icons">
-              <li className="soundcloud"><i className="fa fa-soundcloud" /></li>
-              <li className="youtube"><i className="fa fa-youtube" /></li>
-              <li className="bandcamp"><i className="fa fa-bandcamp" /></li>
-            </ul>
+            <h3 className="flush--top light">Easily add songs from your favorite sources</h3>
             <Link to="Register" className="btn large nudge-half--top">Sign Up</Link>
           </div>
           <div className="scroll-down-container" onClick={this.scrollToInfo}>
@@ -66,14 +61,39 @@ var HomePage = React.createClass({
           <div className="filter" />
         </div>
 
-        <div ref="info" className="info-container">
-          <div className="wrapper soft--ends" style={{ height: '1000px' }}>
+        <div ref="info" className="info-container soft--ends">
+          <div className="wrapper soft--ends">
+            <div className="info-row nudge--bottom text-left">
+              <div className="icon-container soft-half--bottom"><i className="fa fa-users" /></div>
+              <div className="text-container soft--left soft-half--bottom">
+                <h3 className="flush--top">Create playlists collaboratively</h3>
+                <p className="flush--top">Once a playlist is created, the creator has the option of adding other users as 'collaborators'. All collaborators have the ability to add and remove tracks from the playlist, as well as provide feedback on tracks added by other collaborators through per-track upvotes, downvotes, and comments.</p>
+              </div>
+            </div>
+            <div className="info-row nudge--ends text-right">
+              <div className="text-container soft--right soft-half--ends">
+                <h3 className="flush--top">Choose songs from multiple sources</h3>
+                <p className="flush--top">When searching for songs and adding them to your playlists, you have multiple sources to choose from including YouTube, Soundcloud, and Bandcamp. This nearly guarantees that you'll be able to find any song that you have in mind!</p>
+              </div>
+              <div className="icon-container soft-half--ends"><i className="fa fa-plus" /></div>
+            </div>
+            <div className="info-row nudge--ends text-left">
+              <div className="icon-container soft-half--ends"><i className="fa fa-compass" /></div>
+              <div className="text-container soft--left soft-half--ends">
+                <h3 className="flush--top">Discover new playlists and songs</h3>
+                <p className="flush--top">At any time, you can search for new playlists by title or related tags. You can also view playlists that are 'Trending' to see what's popular right now. Lastly, you can also choose to 'Follow' both playlists and users. This ensures that you will receive updates any time a user creates a new playlist, or when new tracks are added to your favorite playlists!</p>
+              </div>
+            </div>
+            <div className="text-center">
+              <Link to="Register" className="btn large">Sign Up</Link>
+            </div>
           </div>
           <div className="shadow" />
         </div>
 
-        <footer>
+        <footer className="soft-half--ends text-left">
           <div className="wrapper">
+            Copyright &copy; {new Date().getFullYear()} Monolist
           </div>
         </footer>
 
