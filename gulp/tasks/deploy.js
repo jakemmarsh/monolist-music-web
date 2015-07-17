2a4419c1b9ab1b7dd6e5e7298301d802a4167784
Jake Marsh <jakemmarsh@gmail.com>
2015-07-17 15:50:09 -0700

    add ebs deploy logic

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -4,12 +4,15 @@ var gulp         = require('gulp');
 var rename       = require('gulp-rename');
 var awspublish   = require('gulp-awspublish');
 var dotenv       = require('dotenv');
+var argv         = require('yargs').argv;
+var shell        = require('gulp-shell');
 var config       = require('../config');
 
 dotenv.load();
 
 gulp.task('deploy', ['prod'], function() {
 
+  var isProd = argv.production || argv.prod;
   var publisher = awspublish.create({
     key: process.env.AWS_KEY,
     secret: process.env.AWS_SECRET,
@@ -19,14 +22,26 @@ gulp.task('deploy', ['prod'], function() {
   var headers = {
     'Cache-Control': 'max-age=' + oneWeekInSeconds + ', no-transform, public'
   };
+  var ebsDeployString = 'ebs-deploy deploy --environment ';
 
-  // Assets
-  return gulp.src(config.buildDir + '**/*.{json,js,css,eot,svg,ttf,woff,otf,png,jpg,jpeg}')
+  // TODO: use zero-downtime deployment for prod ('ebs zdt_deploy')
+  if ( isProd ) {
+    ebsDeployString += 'monolist-app-prod';
+  } else {
+    ebsDeployString += 'monolist-app-dev';
+  }
+
+  // Assets to S3
+  gulp.src(config.buildDir + '**/*.{json,js,css,eot,svg,ttf,woff,otf,png,jpg,jpeg}')
   .pipe(rename(function(path) {
     path.dirname = 'app/' + path.dirname;
   }))
   .pipe(awspublish.gzip())
   .pipe(publisher.publish(headers))
   .pipe(awspublish.reporter());
+
+  // Application to EBS
+  return gulp.src('')
+  .pipe(shell(ebsDeployString));
 
 });
