b38eddaa0d457ea3aa3366b90071974f8e28ffac
Jake Marsh <jake@wealthfront.com>
2015-08-17 12:58:04 -0700

    update imports to match new file structure

--- __tests__/components/Avatar.spec.js
+++ __tests__/components/Avatar.spec.js
@@ -2,7 +2,6 @@
 
 import React       from 'react/addons';
 import {Link}      from 'react-router';
-import Stub        from '../../utils/stubRouterContext';
 import TestHelpers from '../../utils/testHelpers';
 import Avatar      from '../../app/js/components/Avatar';
 const  TestUtils   = React.addons.TestUtils;
@@ -12,7 +11,7 @@ require('../../utils/createAuthenticatedSuite')('Component: Avatar', function() 
   let user = TestHelpers.testUser;
 
   it('should not render a link if props.includeLink is false', done => {
-    let AvatarComponent = Stub(Avatar, { currentUser: user, includeLink: false });
+    let AvatarComponent = TestHelpers.stubRouterContext(Avatar, { currentUser: user, includeLink: false });
     let avatar = TestUtils.renderIntoDocument(React.createElement(AvatarComponent));
     let link = TestUtils.scryRenderedComponentsWithType(avatar, Link);
     console.log('link:', link);
@@ -23,7 +22,7 @@ require('../../utils/createAuthenticatedSuite')('Component: Avatar', function() 
   });
 
   it('should render a link if props.includeLink is true', done => {
-    let AvatarComponent = Stub(Avatar, { currentUser: user, includeLink: true });
+    let AvatarComponent = TestHelpers.stubRouterContext(Avatar, { currentUser: user, includeLink: true });
     let avatar = TestUtils.renderIntoDocument(React.createElement(AvatarComponent));
     let link = TestUtils.scryRenderedComponentsWithType(avatar, Link);
 
@@ -35,7 +34,7 @@ require('../../utils/createAuthenticatedSuite')('Component: Avatar', function() 
   });
 
   it('should set the background image from user info', done => {
-    let AvatarComponent = Stub(Avatar, { currentUser: user });
+    let AvatarComponent = TestHelpers.stubRouterContext(Avatar, { currentUser: user });
     let avatar = TestUtils.renderIntoDocument(React.createElement(AvatarComponent)).getDOMNode();
 
     console.log('background iamge:', avatar.style.backgroundImage);
--- __tests__/components/Header.spec.js
+++ __tests__/components/Header.spec.js
@@ -2,14 +2,13 @@
 
 import React       from 'react/addons';
 import should      from 'should';
-import Stub        from '../../utils/stubRouterContext';
 import TestHelpers from '../../utils/testHelpers';
 const  TestUtils    = React.addons.TestUtils;
 
 require('../../utils/createAuthenticatedSuite')('Component: Header', function() {
 
   let user = TestHelpers.testUser;
-  let HeaderComponent = Stub(require('../../app/js/components/Header'), { currentUser: user });
+  let HeaderComponent = TestHelpers.stubRouterContext(require('../../app/js/components/Header'), { currentUser: user });
   let header;
 
   before(() => {
