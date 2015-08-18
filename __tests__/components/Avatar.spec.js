2c9037b72ba247c9ec42b254da02bb13dba7ac37
Jake Marsh <jakemmarsh@gmail.com>
2015-08-17 20:57:59 -0700

    update tests

--- __tests__/components/Avatar.spec.js
+++ __tests__/components/Avatar.spec.js
@@ -2,43 +2,42 @@
 
 import React       from 'react/addons';
 import {Link}      from 'react-router';
-import Stub        from '../../utils/stubRouterContext';
+
 import TestHelpers from '../../utils/testHelpers';
 import Avatar      from '../../app/js/components/Avatar';
+
 const  TestUtils   = React.addons.TestUtils;
 
-require('../../utils/createAuthenticatedSuite')('Component: Avatar', function() {
+describe('Component: Avatar', function() {
 
-  let user = TestHelpers.testUser;
+  let user = TestHelpers.fixtures.user;
+  let StubbedAvatar = TestHelpers.stubRouterContext(Avatar);
 
-  it('should not render a link if props.includeLink is false', done => {
-    let AvatarComponent = Stub(Avatar, { currentUser: user, includeLink: false });
-    let avatar = TestUtils.renderIntoDocument(React.createElement(AvatarComponent));
+  it('should not render a link if props.includeLink is false', function(done) {
+    let avatar = TestUtils.renderIntoDocument(<StubbedAvatar user={user} includeLink={false} />);
     let link = TestUtils.scryRenderedComponentsWithType(avatar, Link);
+
+    link.should.be.empty();
+
+    done();
+  });
+
+  it('should render a link if props.includeLink is true', function(done) {
+    let avatar = TestUtils.renderIntoDocument(<StubbedAvatar user={user} includeLink={true} />);
+    let link = TestUtils.scryRenderedComponentsWithType(avatar, Link);
+
+    //console.log('avatar:', avatar);
     console.log('link:', link);
-
-    should.not.exist(link);
-
-    done();
-  });
-
-  it('should render a link if props.includeLink is true', done => {
-    let AvatarComponent = Stub(Avatar, { currentUser: user, includeLink: true });
-    let avatar = TestUtils.renderIntoDocument(React.createElement(AvatarComponent));
-    let link = TestUtils.scryRenderedComponentsWithType(avatar, Link);
-
-    console.log('avatar:', avatar.getDOMNode());
-
-    link.should.exist();
+    link.should.have.length.above(0);
 
     done();
   });
 
-  it('should set the background image from user info', done => {
-    let AvatarComponent = Stub(Avatar, { currentUser: user });
-    let avatar = TestUtils.renderIntoDocument(React.createElement(AvatarComponent)).getDOMNode();
+  it('should set the background image from user info', function(done) {
+    let avatar = TestUtils.renderIntoDocument(<StubbedAvatar user={user} includeLink={false} />);
 
-    console.log('background iamge:', avatar.style.backgroundImage);
+    // console.log('dom node:', avatar.getDOMNode());
+    // console.log('background iamge:', avatar.style.backgroundImage);
 
     done();
   });
--- __tests__/components/Header.spec.js
+++ __tests__/components/Header.spec.js
@@ -1,19 +1,18 @@
 'use strict';
 
 import React       from 'react/addons';
-import should      from 'should';
-import Stub        from '../../utils/stubRouterContext';
+
 import TestHelpers from '../../utils/testHelpers';
+
 const  TestUtils    = React.addons.TestUtils;
 
 require('../../utils/createAuthenticatedSuite')('Component: Header', function() {
 
-  let user = TestHelpers.testUser;
-  let HeaderComponent = Stub(require('../../app/js/components/Header'), { currentUser: user });
-  let header;
+  let user = TestHelpers.fixtures.user;
+  let HeaderComponent = TestHelpers.stubRouterContext(require('../../app/js/components/Header'), { currentUser: user });
 
-  before(() => {
-    header = TestUtils.renderIntoDocument(React.createElement(HeaderComponent));
+  before(function() {
+    this.header = TestUtils.renderIntoDocument(React.createElement(HeaderComponent));
   });
 
 });
