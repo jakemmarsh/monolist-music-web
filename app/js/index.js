cebf560940723cfdb016ac62b8f8078547cb1108
Jake Marsh <jake@wealthfront.com>
2015-09-21 13:21:52 -0700

    remove unnecessary react devtools logic

--- app/js/index.js
+++ app/js/index.js
@@ -3,8 +3,4 @@
 import React  from 'react/addons';
 import Routes from './Routes';
 
-if ( process.env.NODE_ENV !== 'production' ) {
-  window.React = React; // Enable React devtools
-}
-
 React.render(Routes, document.getElementById('app'));
