bab94ac3fa2f67e279e78cdf956cdf3e5288d030
Jake Marsh <jake@wealthfront.com>
2015-09-22 17:00:55 -0700

    begin tests for group and playlist sidebars

---
+++ __tests__/components/GroupSidebar.spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React        from 'react/addons';
+import $            from 'jquery';
+
+import TestHelpers  from '../../utils/testHelpers';
+import GroupSidebar from '../../app/js/components/GroupSidebar';
+
+const  TestUtils    = React.addons.TestUtils;
+
+describe('Component: GroupSidebar', function() {
+
+});
---
+++ __tests__/components/PlaylistSidebar.spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React           from 'react/addons';
+import $               from 'jquery';
+
+import TestHelpers     from '../../utils/testHelpers';
+import PlaylistSidebar from '../../app/js/components/PlaylistSidebar';
+
+const  TestUtils       = React.addons.TestUtils;
+
+describe('Component: PlaylistSidebar', function() {
+
+});
