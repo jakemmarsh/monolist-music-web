2a18abc010a748a923823a47bc01f079b29d6870
Jake Marsh <jake@wealthfront.com>
2015-10-06 17:11:52 -0700

    fix last test

--- __tests__/stores/ViewingRecentSearchesStore.spec.js
+++ __tests__/stores/ViewingRecentSearchesStore.spec.js
@@ -4,6 +4,7 @@ import when                       from 'when';
 
 import ViewingRecentSearchesStore from '../../app/js/stores/ViewingRecentSearchesStore';
 import GlobalActions              from '../../app/js/actions/GlobalActions';
+import PostAPI                    from '../../app/js/utils/PostAPI';
 import PlaylistAPI                from '../../app/js/utils/PlaylistAPI';
 
 describe('Store: ViewingRecentSearches', function() {
@@ -12,12 +13,10 @@ describe('Store: ViewingRecentSearches', function() {
     this.playlistApiMock = sandbox.mock(PlaylistAPI);
   });
 
-  it('should load recent playlist searches on action', function(done) {
+  it('should load recent playlist searches on action', function() {
     this.playlistApiMock.expects('getRecentSearches').returns(when());
-
+    sandbox.stub(PostAPI, 'getNewest').returns(when()); // Since ViewingPostListStore is also triggered by action
     GlobalActions.loadExplorePage();
-
-    done();
   });
 
 });
