e3f29f282db7c535230e34201748383e5e141df7
Jake Marsh <jakemmarsh@gmail.com>
2015-11-08 18:42:56 -0800

    no global mock

--- __tests__/stores/GroupSearchStore.spec.js
+++ __tests__/stores/GroupSearchStore.spec.js
@@ -8,14 +8,10 @@ import SearchAPI        from '../../app/js/utils/SearchAPI';
 
 describe('Store: GroupSearch', function() {
 
-  beforeEach(function() {
-    this.searchAPIMock = sandbox.mock(SearchAPI);
-  });
-
   it('should search all groups on action', function(done) {
     let query = 'test';
 
-    this.searchAPIMock.expects('groupSearch').withArgs(query).returns(when());
+    sandbox.mock(SearchAPI).expects('groupSearch').withArgs(query).returns(when());
 
     SearchActions.searchGroups(query);
 
