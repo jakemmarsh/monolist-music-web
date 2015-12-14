6a452ad04aeb564969612b19dddadbae45ad0934
Jake Marsh <jakemmarsh@gmail.com>
2015-12-13 18:27:29 -0800

    clean up user store tests

--- __tests__/stores/CurrentUserStore.spec.js
+++ __tests__/stores/CurrentUserStore.spec.js
@@ -11,94 +11,76 @@ import TrackAPI         from '../../app/js/utils/TrackAPI';
 
 describe('Store: CurrentUser', function() {
 
-
-  beforeEach(function() {
-    CurrentUserStore.init();
-    this.userAPIMock = sandbox.mock(UserAPI);
-    this.authAPIMock = sandbox.mock(AuthAPI);
-    this.trackAPIMock = sandbox.mock(TrackAPI);
-  });
-
-  it('should be empty on init', function() {
-    CurrentUserStore.user.should.eql({});
-  });
-
   it('should check user\'s login status on action', function() {
-    this.authAPIMock.expects('check').returns(when({}));
+    sandbox.mock(AuthAPI).expects('check').returns(when({}));
 
     UserActions.check();
   });
 
-  it('should log user in on action', function(done) {
+  it('should log user in on action', function() {
     let user = {
       username: 'test',
       password: 'test'
     };
 
-    this.authAPIMock.expects('login').withArgs(user).returns(when({}));
+    sandbox.mock(AuthAPI).expects('login').withArgs(user).returns(when({}));
 
     UserActions.login(user);
-
-    done();
   });
 
-  it('should log user in via facebook on action', function(done) {
+  it('should log user in via facebook on action', function() {
     let user = {
-      access_token: 'abcdefg',
+      access_token: 'abcdefg', //eslint-disable-line camelcase
       profile: {}
     };
 
-    this.authAPIMock.expects('facebookLogin').withArgs(user).returns(when({}));
+    sandbox.mock(AuthAPI).expects('facebookLogin').withArgs(user).returns(when({}));
 
     UserActions.facebookLogin(user);
-
-    done();
   });
 
-  it('should update a user on action', function(done) {
-    let userId = 1;
+  it('should update a user on action', function() {
+    let user = {
+      id: 1
+    };
     let updates = {
       email: 'new@test.com'
     };
 
-    this.userAPIMock.expects('update').withArgs(userId, updates).returns(when({}));
+    sandbox.mock(UserAPI).expects('update').withArgs(user.id, updates).returns(when({}));
 
-    CurrentUserStore.user.id = userId;
+    CurrentUserStore.user = user;
     UserActions.update(updates);
-
-    done();
   });
 
-  it('should star a track on action', function(done) {
+  it('should star a track on action', function() {
     let track = {
       id: 1,
       title: 'test'
     };
 
     CurrentUserStore.user = { starredTracks: [] };
-    this.trackAPIMock.expects('star').withArgs(track).returns(when());
+    sandbox.mock(TrackAPI).expects('star').withArgs(track).returns(when());
 
-    TrackActions.star(track, done);
+    TrackActions.star(track);
   });
 
-  it('should unstar a track on action', function(done) {
+  it('should unstar a track on action', function() {
     let track = {
       id: 1,
       title: 'test'
     };
 
     CurrentUserStore.user = { starredTracks: [] };
-    this.trackAPIMock.expects('star').withArgs(track).returns(when());
+    sandbox.mock(TrackAPI).expects('star').withArgs(track).returns(when());
 
-    TrackActions.star(track, done);
+    TrackActions.star(track);
   });
 
-  it('should log user out on action', function(done) {
-    this.authAPIMock.expects('logout').once().returns(when());
+  it('should log user out on action', function() {
+    sandbox.mock(AuthAPI).expects('logout').once().returns(when());
 
     UserActions.logout();
-
-    done();
   });
 
 });
