d2d96ff853e58b3818f2e0a4437c2f275785eec9
Jake Marsh <jakemmarsh@gmail.com>
2015-12-13 19:12:13 -0800

    fix http stubbing

--- __tests__/helper.js
+++ __tests__/helper.js
@@ -2,13 +2,21 @@
 
 import request from 'superagent';
 
+function httpStub() {
+  return {
+    withCredentials: () => {
+      return { end: () => {} };
+    }
+  };
+};
+
 beforeEach(function() {
   global.sandbox = sinon.sandbox.create();
-  global.sandbox.stub(request, 'get');
-  global.sandbox.stub(request, 'put');
-  global.sandbox.stub(request, 'patch');
-  global.sandbox.stub(request, 'post');
-  global.sandbox.stub(request, 'del');
+  global.sandbox.stub(request, 'get', httpStub);
+  global.sandbox.stub(request, 'put', httpStub);
+  global.sandbox.stub(request, 'patch', httpStub);
+  global.sandbox.stub(request, 'post', httpStub);
+  global.sandbox.stub(request, 'del', httpStub);
 });
 
 afterEach(function() {
