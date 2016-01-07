3bf45032718a2dbbc53e3d40c93ddf79d424a0a2
Jake Marsh <jake@wealthfront.com>
2016-01-07 15:52:10 -0800

    remove unused methods

--- app/js/components/NavigationSidebar.js
+++ app/js/components/NavigationSidebar.js
@@ -18,51 +18,6 @@ var NavigationSidebar = React.createClass({
     };
   },
 
-  renderMyPlaylistsLink() {
-    if ( !_.isEmpty(this.props.currentUser) ) {
-      return (
-        <ListLink to="/playlists">
-          <div className="icon-container">
-            <i className="icon-list"></i>
-          </div>
-          <div className="text-container">
-            My Playlists
-          </div>
-        </ListLink>
-      );
-    }
-  },
-
-  renderCreatePlaylistLink() {
-    if ( !_.isEmpty(this.props.currentUser) ) {
-      return (
-        <ListLink to="/playlists/create">
-          <div className="icon-container">
-            <i className="icon-plus"></i>
-          </div>
-          <div className="text-container">
-            Create Playlist
-          </div>
-        </ListLink>
-      );
-    }
-  },
-
-  renderCreateGroupLink() {
-    if ( !_.isEmpty(this.props.currentUser) ) {
-      return (
-        <ListLink to="/groups/create">
-          <div className="icon-container">
-            <i className="icon-user-plus"></i>
-          </div>
-          <div className="text-container">
-            Create Group
-          </div>
-        </ListLink>
-      );
-    }
-  },
-
   renderLinks() {
     return _.map(navLinks, (link, index) => {
       if ( !link.requiresUser || !_.isEmpty(this.props.currentUser) ) {
