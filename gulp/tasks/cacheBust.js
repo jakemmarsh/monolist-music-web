44b71404ca7515bbbf591b31640bdb5e9b7b77a4
Jake Marsh <jakemmarsh@gmail.com>
2016-01-14 22:07:07 -0800

    fix cache busting to not break mixpanel code

--- gulp/tasks/cacheBust.js
+++ gulp/tasks/cacheBust.js
@@ -2,6 +2,7 @@
 
 import gulp      from 'gulp';
 import cacheBust from 'gulp-cache-bust';
+import replace   from 'gulp-replace';
 import config    from '../config';
 
 gulp.task('cacheBust', () => {
@@ -10,6 +11,9 @@ gulp.task('cacheBust', () => {
   .pipe(cacheBust({
     type: 'timestamp'
   }))
+  // hack to ensure that Mixpanel's code doesn't get fucked up
+  // TODO: fix this
+  .pipe(replace(/undefined(\?t=[\d]*)+/gi, 'undefined'))
   .pipe(gulp.dest(config.buildDir));
 
 });
