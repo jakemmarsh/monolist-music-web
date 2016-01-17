23f96780d9334fa4b9f0bf23ea2092beccfcfdc2
Jake Marsh <jakemmarsh@gmail.com>
2016-01-17 00:21:53 -0800

    fix async tests

--- __tests__/stores/ModalStore.spec.js
+++ __tests__/stores/ModalStore.spec.js
@@ -7,21 +7,23 @@ import GlobalActions from '../../app/js/actions/GlobalActions';
 
 describe('Store: Modal', function() {
 
-  it('should set modal class and contents then trigger on open action', function() {
+  it('should set modal class and contents then trigger on open action', function(done) {
     const modalClass = 'test-class';
     const modalContents = (<div />);
     const triggerStub = sandbox.stub(ModalStore, 'trigger', () => {
       sinon.assert.calledOnce(triggerStub);
       sinon.assert.calledWith(triggerStub, modalClass, modalContents);
+      done();
     });
 
     GlobalActions.openModal(modalClass, modalContents);
   });
 
-  it('should reset modal class and contents then trigger on close action', function() {
+  it('should reset modal class and contents then trigger on close action', function(done) {
     const triggerStub = sandbox.stub(ModalStore, 'trigger', () => {
       sinon.assert.calledOnce(triggerStub);
       sinon.assert.calledWith(triggerStub, null, null);
+      done();
     });
 
     GlobalActions.closeModal();
