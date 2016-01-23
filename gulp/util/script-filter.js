a26c3abe66d37a67dfc98478e214cc2a386bbb21
Jake Marsh <jakemmarsh@gmail.com>
2016-01-22 22:15:23 -0800

    fix for babel

--- gulp/util/script-filter.js
+++ gulp/util/script-filter.js
@@ -1,9 +1,9 @@
 'use strict';
 
-import path from 'path';
+var path = require('path');
 
 // Filters out non .coffee and .js files. Prevents
 // accidental inclusion of possible hidden files
-export default function(name) {
+module.exports = function(name) {
   return /(\.(js|coffee)$)/i.test(path.extname(name));
 };
