cee328b074df02b9b3e4fb112f60f253dde2aa45
Jake Marsh <jakemmarsh@gmail.com>
2016-01-25 21:41:36 -0800

    use blurred background image instead of color analysis

--- app/js/components/CurrentlyPlaying.js
+++ app/js/components/CurrentlyPlaying.js
@@ -1,12 +1,11 @@
 'use strict';
 
-import React               from 'react';
-import $                   from 'jquery';
-import cx                  from 'classnames';
-import _                   from 'lodash';
+import React           from 'react';
+import $               from 'jquery';
+import cx              from 'classnames';
+import _               from 'lodash';
 
-import AudioControlBar     from './AudioControlBar';
-import ImageAnalyzer       from '../utils/ImageAnalyzer';
+import AudioControlBar from './AudioControlBar';
 
 var CurrentlyPlaying = React.createClass({
 
@@ -33,21 +32,8 @@ var CurrentlyPlaying = React.createClass({
 
   getInitialState() {
     return {
-      isFull: this.props.currentTrack !== null,
-      colors: null
+      isFull: this.props.currentTrack !== null
     };
-  },
-
-  _determineColors(nextProps) {
-    const hasTrack = !_.isEmpty(nextProps.currentTrack);
-    const isNewTrack = _.isEmpty(this.props.currentTrack) || !_.isEqual(this.props.currentTrack, nextProps.currentTrack);
-    const trackHasArt = hasTrack ? !_.isEmpty(nextProps.currentTrack.imageUrl) : false;
-
-    if ( hasTrack && isNewTrack && trackHasArt ) {
-      this.calculateTrackColors(nextProps.currentTrack);
-    } else if ( !hasTrack || !trackHasArt && this.state.colors !== null ) {
-      this.setState({ colors: null });
-    }
   },
 
   _displayArtOrVideo() {
@@ -64,38 +50,8 @@ var CurrentlyPlaying = React.createClass({
     }
   },
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({ isFull: nextProps.currentTrack !== null }, this.changeMainContentWrapperClass);
-    this._determineColors(nextProps);
-  },
-
   componentDidUpdate() {
     this._displayArtOrVideo();
-  },
-
-  calculateTrackColors(nextTrack) {
-    let image = new Image();
-    let tempColors;
-
-    image.addEventListener('load', () => {
-      tempColors = ImageAnalyzer.extractImageColors(image, 'css');
-      this.setState({
-        colors: {
-          background: tempColors.background,
-          primaryText: tempColors.content[0],
-          secondaryText: tempColors.content[1] || null
-        }
-      });
-    });
-
-    image.addEventListener('error', () => {
-      // Image didn't load, reset colors
-      this.setState({ colors: null });
-    });
-
-    image.crossOrigin = 'Anonymous';
-    // TODO: better solution than this pass-through?
-    image.src = 'http://cors-anywhere.herokuapp.com/' + nextTrack.imageUrl;
   },
 
   changeMainContentWrapperClass() {
@@ -107,55 +63,34 @@ var CurrentlyPlaying = React.createClass({
   },
 
   renderTitle() {
-    let element = null;
-    let styles = {};
-
-    if ( this.state.colors && this.state.colors.primaryText ) {
-      styles.color = this.state.colors.primaryText;
-    }
-
     if ( this.props.currentTrack && this.props.currentTrack.title ) {
-      element = (
-        <h1 className="title" style={styles}>{this.props.currentTrack.title}</h1>
+      return (
+        <h1 className="title">{this.props.currentTrack.title}</h1>
       );
     }
-
-    return element;
   },
 
   renderArtist() {
-    let element = null;
-    let styles = {};
-
-    if ( this.state.colors && this.state.colors.secondaryText ) {
-      styles.color = this.state.colors.secondaryText;
-    }
-
     if ( this.props.currentTrack && this.props.currentTrack.artist ) {
-      element = (
-        <h5 className="artist" style={styles}>{this.props.currentTrack.artist}</h5>
+      return (
+        <h5 className="artist">{this.props.currentTrack.artist}</h5>
       );
     }
-
-    return element;
   },
 
   render() {
+    const hasImage = this.props.currentTrack && this.props.currentTrack.imageUrl;
     const classes = cx({
       'currently-playing': true,
+      'has-background': hasImage,
       'full': this.state.isFull
     });
     const artworkStyles = {
-      'backgroundImage': this.props.currentTrack && this.props.currentTrack.source !== 'youtube' ? 'url(' + this.props.currentTrack.imageUrl + ')' : null
+      'backgroundImage': hasImage ? 'url(' + this.props.currentTrack.imageUrl + ')' : null
     };
-    let styles = {};
-
-    if ( this.state.colors && this.state.colors.background ) {
-      styles.backgroundColor = this.state.colors.background;
-    }
 
     return (
-      <div className={classes} style={styles}>
+      <div className={classes}>
 
         <div className="artwork-info-container">
           <div className="image-video-container soft-quarter--ends">
@@ -172,7 +107,6 @@ var CurrentlyPlaying = React.createClass({
         <AudioControlBar ref="controlBar"
                          currentUser={this.props.currentUser}
                          userCollaborations={this.props.userCollaborations}
-                         colors={this.state.colors}
                          player={this.props.player}
                          audio={this.props.audio}
                          currentTrack={this.props.currentTrack}
@@ -190,6 +124,8 @@ var CurrentlyPlaying = React.createClass({
                          toggleRepeat={this.props.toggleRepeat}
                          toggleShuffle={this.props.toggleShuffle} />
 
+        <div className="player-background-image" style={artworkStyles} />
+
       </div>
     );
   }
@@ -197,9 +133,3 @@ var CurrentlyPlaying = React.createClass({
 });
 
 export default CurrentlyPlaying;
-
-// <div className="player-toggle" onClick={this.toggleMinimizePlayer}>
-//   <hr />
-//   <hr />
-//   <hr />
-// </div>
