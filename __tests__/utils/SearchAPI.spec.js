fde26ea2b6c27ac7a2d09be9e7eb537955f41b7a
Jake Marsh <jakemmarsh@gmail.com>
2016-01-25 22:28:48 -0800

    cache search results for 10 minutes

--- __tests__/utils/SearchAPI.spec.js
+++ __tests__/utils/SearchAPI.spec.js
@@ -1,68 +1,173 @@
 'use strict';
 
+import when      from 'when';
+import lscache   from 'lscache';
+
 import APIUtils  from '../../app/js/utils/APIUtils';
 import SearchAPI from '../../app/js/utils/SearchAPI';
 
 describe('Util: SearchAPI', function() {
 
-  beforeEach(function() {
-    this.apiUtilsMock = sandbox.mock(APIUtils);
+  const results = [{ id: 1 }, { id: 2 }];
+
+  describe('#trackSearch', function() {
+    describe('when key doesn\'t exist in cache', function() {
+      beforeEach(function() {
+        sandbox.stub(lscache, 'get').returns(null);
+      });
+
+      it('should make a request to search all tracks defaulting to all sources and store in cache', function(done) {
+        const query = 'test';
+        const path = 'tracks/search/' + query + '?sources=soundcloud,youtube,bandcamp';
+        const key = `trackSearch:${query}:soundcloud,youtube,bandcamp`;
+        const getStub = sandbox.stub(APIUtils, 'get').returns(when(results));
+        const setStub = sandbox.stub(lscache, 'set');
+
+        SearchAPI.trackSearch(query).then(() => {
+          sinon.assert.calledWith(getStub, path);
+          sinon.assert.calledWith(setStub, key, results);
+          done();
+        });
+      });
+
+      it('should take a variable list of sources for track search and store in cache', function(done) {
+        const query = 'test';
+        const sources = ['soundcloud', 'bandcamp'];
+        const key = `trackSearch:${query}:${sources.join(',')}`;
+        const path = 'tracks/search/' + query + '?sources=soundcloud,bandcamp';
+        const getStub = sandbox.stub(APIUtils, 'get').returns(when(results));
+        const setStub = sandbox.stub(lscache, 'set');
+
+        SearchAPI.trackSearch(query, sources).then(() => {
+          sinon.assert.calledWith(getStub, path);
+          sinon.assert.calledWith(setStub, key, results);
+          done();
+        });
+      });
+    });
+
+    describe('when key does exist in cache', function() {
+      it('should retrieve the results from the cache', function(done) {
+        const query = 'test';
+        const sources = ['soundcloud', 'bandcamp'];
+        const key = `trackSearch:${query}:${sources.join(',')}`;
+
+        sandbox.stub(lscache, 'get').withArgs(key).returns(results);
+
+        SearchAPI.trackSearch(query, sources).then((searchResults) => {
+          searchResults.should.eql(results);
+          done();
+        });
+      });
+    });
   });
 
-  it('should make a request to search all tracks defaulting to all sources', function(done) {
-    let query = 'test';
-    let path = 'tracks/search/' + query + '?sources=soundcloud,youtube,bandcamp';
+  describe('#userSearch', function() {
+    describe('when key doesn\'t exist in cache', function() {
+      beforeEach(function() {
+        sandbox.stub(lscache, 'get').returns(null);
+      });
 
-    this.apiUtilsMock.expects('get').withArgs(path);
+      it('should make a request to search all users and store in cache', function(done) {
+        const query = 'test';
+        const path = 'users/search/' + query;
+        const key = `userSearch:${query}`;
+        const getStub = sandbox.stub(APIUtils, 'get').returns(when(results));
+        const setStub = sandbox.stub(lscache, 'set');
 
-    SearchAPI.trackSearch(query);
+        SearchAPI.userSearch(query).then(() => {
+          sinon.assert.calledWith(getStub, path);
+          sinon.assert.calledWith(setStub, key, results);
+          done();
+        });
+      });
+    });
 
-    done();
+    describe('when key does exist in cache', function() {
+      it('should retrieve the results from the cache', function(done) {
+        const query = 'test';
+        const key = `userSearch:${query}`;
+
+        sandbox.stub(lscache, 'get').withArgs(key).returns(results);
+
+        SearchAPI.userSearch(query).then((searchResults) => {
+          searchResults.should.eql(results);
+          done();
+        });
+      });
+    });
   });
 
-  it('should take a variable list of sources for track search', function(done) {
-    let query = 'test';
-    let sources = ['soundcloud', 'bandcamp'];
-    let path = 'tracks/search/' + query + '?sources=soundcloud,bandcamp';
+  describe('#playlistSearch', function() {
+    describe('when key doesn\'t exist in cache', function() {
+      beforeEach(function() {
+        sandbox.stub(lscache, 'get').returns(null);
+      });
 
-    this.apiUtilsMock.expects('get').withArgs(path);
+      it('should make a request to search all playlists and store in cache', function(done) {
+        const query = 'test';
+        const path = 'playlists/search/' + query;
+        const key = `playlistSearch:${query}`;
+        const getStub = sandbox.stub(APIUtils, 'get').returns(when(results));
+        const setStub = sandbox.stub(lscache, 'set');
 
-    SearchAPI.trackSearch(query, sources);
+        SearchAPI.playlistSearch(query).then(() => {
+          sinon.assert.calledWith(getStub, path);
+          sinon.assert.calledWith(setStub, key, results);
+          done();
+        });
+      });
+    });
 
-    done();
+    describe('when key does exist in cache', function() {
+      it('should retrieve the results from the cache', function(done) {
+        const query = 'test';
+        const key = `playlistSearch:${query}`;
+
+        sandbox.stub(lscache, 'get').withArgs(key).returns(results);
+
+        SearchAPI.playlistSearch(query).then((searchResults) => {
+          searchResults.should.eql(results);
+          done();
+        });
+      });
+    });
   });
 
-  it('should make a request to search all users', function(done) {
-    let query = 'test';
-    let path = 'users/search/' + query;
+  describe('#groupSearch', function() {
+    describe('when key doesn\'t exist in cache', function() {
+      beforeEach(function() {
+        sandbox.stub(lscache, 'get').returns(null);
+      });
 
-    this.apiUtilsMock.expects('get').withArgs(path);
+      it('should make a request to search all groups and store in cache', function(done) {
+        const query = 'test';
+        const path = 'groups/search/' + query;
+        const key = `groupSearch:${query}`;
+        const getStub = sandbox.stub(APIUtils, 'get').returns(when(results));
+        const setStub = sandbox.stub(lscache, 'set');
 
-    SearchAPI.userSearch(query);
+        SearchAPI.groupSearch(query).then(() => {
+          sinon.assert.calledWith(getStub, path);
+          sinon.assert.calledWith(setStub, key, results);
+          done();
+        });
+      });
+    });
 
-    done();
-  });
+    describe('when key does exist in cache', function() {
+      it('should retrieve the results from the cache', function(done) {
+        const query = 'test';
+        const key = `groupSearch:${query}`;
 
-  it('should make a request to search all playlists', function(done) {
-    let query = 'test';
-    let path = 'playlists/search/' + query;
+        sandbox.stub(lscache, 'get').withArgs(key).returns(results);
 
-    this.apiUtilsMock.expects('get').withArgs(path);
-
-    SearchAPI.playlistSearch(query);
-
-    done();
-  });
-
-  it('should make a request to search all groups', function(done) {
-    let query = 'test';
-    let path = 'groups/search/' + query;
-
-    this.apiUtilsMock.expects('get').withArgs(path);
-
-    SearchAPI.groupSearch(query);
-
-    done();
+        SearchAPI.groupSearch(query).then((searchResults) => {
+          searchResults.should.eql(results);
+          done();
+        });
+      });
+    });
   });
 
 });
--- app/js/utils/SearchAPI.js
+++ app/js/utils/SearchAPI.js
@@ -1,29 +1,89 @@
 'use strict';
 
+import lscache  from 'lscache';
+
 import APIUtils from './APIUtils';
+
+const CACHE_TIME_IN_MINUTES = 10;
 
 var SearchAPI = {
 
   trackSearch(query, sources) {
-    if ( sources && sources.length ) {
-      sources = sources.join(',');
-    } else {
-      sources = 'soundcloud,youtube,bandcamp';
-    }
+    return new Promise((resolve, reject) => {
+      if ( sources && sources.length ) {
+        sources = sources.join(',');
+      } else {
+        sources = 'soundcloud,youtube,bandcamp';
+      }
 
-    return APIUtils.get(`tracks/search/${encodeURIComponent(query)}?sources=${sources}`);
+      const key = `trackSearch:${query}:${sources}`;
+      const cached = lscache.get(key);
+
+      if ( cached ) {
+        resolve(cached);
+      } else {
+        APIUtils.get(`tracks/search/${encodeURIComponent(query)}?sources=${sources}`).then((results) => {
+          lscache.set(key, results, CACHE_TIME_IN_MINUTES);
+          resolve(results);
+        }).catch((err) => {
+          reject(err);
+        });
+      }
+    });
   },
 
   userSearch(query) {
-    return APIUtils.get(`users/search/${encodeURIComponent(query)}`);
+    return new Promise((resolve, reject) => {
+      const key = `userSearch:${query}`;
+      const cached = lscache.get(key);
+
+      if ( cached ) {
+        resolve(cached);
+      } else {
+        APIUtils.get(`users/search/${encodeURIComponent(query)}`).then((results) => {
+          lscache.set(key, results, CACHE_TIME_IN_MINUTES);
+          resolve(results);
+        }).catch((err) => {
+          reject(err);
+        });
+      }
+    });
   },
 
   playlistSearch(query) {
-    return APIUtils.get(`playlists/search/${encodeURIComponent(query)}`);
+    return new Promise((resolve, reject) => {
+      const key = `playlistSearch:${query}`;
+      const cached = lscache.get(key);
+
+      if ( cached ) {
+        resolve(cached);
+      } else {
+        APIUtils.get(`playlists/search/${encodeURIComponent(query)}`).then((results) => {
+          lscache.set(key, results, CACHE_TIME_IN_MINUTES);
+          resolve(results);
+        }).catch((err) => {
+          reject(err);
+        });
+      }
+    });
   },
 
   groupSearch(query) {
-    return APIUtils.get(`groups/search/${encodeURIComponent(query)}`);
+    return new Promise((resolve, reject) => {
+      const key = `groupSearch:${query}`;
+      const cached = lscache.get(key);
+
+      if ( cached ) {
+        resolve(cached);
+      } else {
+        APIUtils.get(`groups/search/${encodeURIComponent(query)}`).then((results) => {
+          lscache.set(key, results, CACHE_TIME_IN_MINUTES);
+          resolve(results);
+        }).catch((err) => {
+          reject(err);
+        });
+      }
+    });
   }
 
 };
