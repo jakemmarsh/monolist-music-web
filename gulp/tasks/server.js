a7b041e0487f554de5cb01d5d7fd5fb245b47960
Jake Marsh <jakemmarsh@gmail.com>
2016-02-14 12:01:14 -0800

    es6

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var config  = require('../config');
-var http    = require('http');
-var express = require('express');
-var gulp    = require('gulp');
-var gutil   = require('gulp-util');
-var morgan  = require('morgan');
+import config  from '../config';
+import http    from 'http';
+import express from 'express';
+import gulp    from 'gulp';
+import gutil   from 'gulp-util';
+import morgan  from 'morgan';
 
-gulp.task('server', function() {
+gulp.task('server', () => {
 
   var server = express();
 
@@ -21,13 +21,13 @@ gulp.task('server', function() {
   server.use('*/node_modules', express.static('./node_modules'));
 
   // Serve index.html for all routes to leave routing up to Angular
-  server.all('/*', function(req, res) {
+  server.all('/*', (req, res) => {
       res.sendFile('index.html', { root: 'build' });
   });
 
   // Start webserver if not already running
   var s = http.createServer(server);
-  s.on('error', function(err){
+  s.on('error', (err) => {
     if(err.code === 'EADDRINUSE'){
       gutil.log('Development server is already started at port ' + config.serverPort);
     }
