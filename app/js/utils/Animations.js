0fab7665b46663891fc839aa106bbff68fd31569
Jake Marsh <jakemmarsh@gmail.com>
2016-02-16 23:50:20 -0800

    add vanilla JS animations

---
+++ app/js/utils/Animations.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const Animations = {
+
+  fadeIn(elem, duration = 500) {
+    return new Promise((resolve) => {
+      let opacity = 0;
+
+      elem.style.opacity = opacity;
+      elem.style.display = 'inline-block';
+
+      let timer = setInterval(() => {
+        if ( opacity >= 1 ) {
+          clearInterval(timer);
+          resolve();
+        } else {
+          opacity += 0.1;
+          elem.style.opacity = opacity;
+        }
+      }, duration/10);
+    });
+  },
+
+  fadeOut(elem, duration) {
+    return new Promise((resolve) => {
+      let opacity = elem.style.opacity;
+
+      let timer = setInterval(() => {
+        if ( opacity <= 0 ) {
+          elem.style.display = 'none';
+          clearInterval(timer);
+          resolve();
+        } else {
+          opacity -= 0.1;
+          elem.style.opacity = opacity;
+        }
+      }, duration/10);
+    });
+  }
+
+};
+
+export default Animations;
