1870afdace0984cc8631da3ea42231641306a38c
Jake Marsh <jakemmarsh@gmail.com>
2016-02-27 15:20:37 -0800

    code cleanup

--- app/js/stores/CurrentTrackStore.js
+++ app/js/stores/CurrentTrackStore.js
@@ -1,25 +1,28 @@
 'use strict';
 
-import Reflux           from 'reflux';
+import Reflux       from 'reflux';
 
-import TrackActions     from '../actions/TrackActions';
-import Mixpanel         from '../utils/Mixpanel';
+import TrackActions from '../actions/TrackActions';
+import Mixpanel     from '../utils/Mixpanel';
 
 var CurrentTrackStore = Reflux.createStore({
 
   init() {
+    this.track = null;
+    this.currentIndex = null;
+
     this.listenTo(TrackActions.select, this.selectTrack);
   },
 
-  selectTrack(track, index, cb = function() {}) {
+  selectTrack(track, index) {
     this.track = track;
     this.currentIndex = index;
+
     Mixpanel.logEvent('play track', {
       track: track
     });
 
     this.trigger(track, index);
-    cb(track, index);
   }
 
 });
