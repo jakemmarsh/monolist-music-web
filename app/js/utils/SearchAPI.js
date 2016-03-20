4767e6bed42942d04077a355a20ea93ebf3a2bf4
Jake Marsh <jakemmarsh@gmail.com>
2016-03-20 11:22:21 -0700

    don't cache search results on client

--- app/js/utils/SearchAPI.js
+++ app/js/utils/SearchAPI.js
@@ -1,10 +1,6 @@
 'use strict';
 
-import lscache  from 'lscache';
-
 import APIUtils from './APIUtils';
-
-const CACHE_TIME_IN_MINUTES = 10;
 
 var SearchAPI = {
 
@@ -16,73 +12,41 @@ var SearchAPI = {
         sources = 'soundcloud,youtube,bandcamp';
       }
 
-      const key = `trackSearch:${query}:${sources}`;
-      const cached = lscache.get(key);
-
-      if ( cached ) {
-        resolve(cached);
-      } else {
-        APIUtils.get(`tracks/search/${encodeURIComponent(query)}?sources=${sources}`).then((results) => {
-          lscache.set(key, results, CACHE_TIME_IN_MINUTES);
-          resolve(results);
-        }).catch((err) => {
-          reject(err);
-        });
-      }
+      APIUtils.get(`tracks/search/${encodeURIComponent(query)}?sources=${sources}`).then((results) => {
+        resolve(results);
+      }).catch((err) => {
+        reject(err);
+      });
     });
   },
 
   userSearch(query) {
     return new Promise((resolve, reject) => {
-      const key = `userSearch:${query}`;
-      const cached = lscache.get(key);
-
-      if ( cached ) {
-        resolve(cached);
-      } else {
-        APIUtils.get(`users/search/${encodeURIComponent(query)}`).then((results) => {
-          lscache.set(key, results, CACHE_TIME_IN_MINUTES);
-          resolve(results);
-        }).catch((err) => {
-          reject(err);
-        });
-      }
+      APIUtils.get(`users/search/${encodeURIComponent(query)}`).then((results) => {
+        resolve(results);
+      }).catch((err) => {
+        reject(err);
+      });
     });
   },
 
   playlistSearch(query) {
     return new Promise((resolve, reject) => {
-      const key = `playlistSearch:${query}`;
-      const cached = lscache.get(key);
-
-      if ( cached ) {
-        resolve(cached);
-      } else {
-        APIUtils.get(`playlists/search/${encodeURIComponent(query)}`).then((results) => {
-          lscache.set(key, results, CACHE_TIME_IN_MINUTES);
-          resolve(results);
-        }).catch((err) => {
-          reject(err);
-        });
-      }
+      APIUtils.get(`playlists/search/${encodeURIComponent(query)}`).then((results) => {
+        resolve(results);
+      }).catch((err) => {
+        reject(err);
+      });
     });
   },
 
   groupSearch(query) {
     return new Promise((resolve, reject) => {
-      const key = `groupSearch:${query}`;
-      const cached = lscache.get(key);
-
-      if ( cached ) {
-        resolve(cached);
-      } else {
-        APIUtils.get(`groups/search/${encodeURIComponent(query)}`).then((results) => {
-          lscache.set(key, results, CACHE_TIME_IN_MINUTES);
-          resolve(results);
-        }).catch((err) => {
-          reject(err);
-        });
-      }
+      APIUtils.get(`groups/search/${encodeURIComponent(query)}`).then((results) => {
+        resolve(results);
+      }).catch((err) => {
+        reject(err);
+      });
     });
   }
 
