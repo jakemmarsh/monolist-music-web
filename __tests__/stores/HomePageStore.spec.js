c9bfa9aeebe6bde108217a339faa22ee4d9a0bd7
Jake Marsh <jakemmarsh@gmail.com>
2016-04-17 13:07:29 -0700

    fix test

--- __tests__/stores/HomePageStore.spec.js
+++ __tests__/stores/HomePageStore.spec.js
@@ -9,13 +9,13 @@ describe('Store: HomePage', function() {
   it('should load trending and top monthly playlists on action', function(done) {
     const userId = 3;
 
-    sandbox.stub(PlaylistAPI, 'getTrending').resolves();
+    sandbox.stub(PlaylistAPI, 'getUserRecentlyPlayed').resolves();
     sandbox.stub(PlaylistAPI, 'getGlobalRecentlyPlayed').resolves();
     sandbox.stub(PlaylistAPI, 'getNewest').resolves();
 
     GlobalActions.loadHomePage(userId, () => {
       sinon.assert.calledOnce(PlaylistAPI.getUserRecentlyPlayed);
-      sinon.assert.calledWith(PlaylistAPI.getUserRecentlyPlayed, userId, sinon.match.func);
+      sinon.assert.calledWith(PlaylistAPI.getUserRecentlyPlayed, userId);
       sinon.assert.calledOnce(PlaylistAPI.getGlobalRecentlyPlayed);
       sinon.assert.calledOnce(PlaylistAPI.getNewest);
       done();
