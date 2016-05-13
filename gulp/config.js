8acf32ae062993ba1d8170752f5e68f95340d913
Jake Marsh <jakemmarsh@gmail.com>
2016-05-12 20:54:23 -0700

    clean up gulp config

--- gulp/config.js
+++ gulp/config.js
@@ -2,37 +2,37 @@
 
 const config = {
 
-  'serverPort': 3001,
-  'browserPort': 3002,
-  'UIPort': 3003,
+  serverPort: 3001,
+  browserPort: 3002,
+  UIPort: 3003,
 
-  'scripts': {
-    'src': './app/js/**/*.js',
-    'dest': './build/js/',
-    'test': './tests/**/*.js',
-    'gulp': './gulp/**/*.js'
+  scripts: {
+    src: './app/js/**/*.js',
+    dest: './build/js/',
+    test: './tests/**/*.js',
+    gulp: './gulp/**/*.js'
   },
 
-  'images': {
-    'src': './app/images/**/*.{jpeg,jpg,png}',
-    'dest': './build/images/'
+  images: {
+    src: './app/images/**/*.{jpeg,jpg,png}',
+    dest: './build/images/'
   },
 
-  'styles': {
-    'src': './app/styles/**/*.scss',
-    'dest': './build/css/'
+  styles: {
+    src: './app/styles/**/*.scss',
+    dest: './build/css/'
   },
 
-  'fonts': {
-    'src': ['./app/fonts/**/*'],
-    'dest': './build/fonts'
+  fonts: {
+    src: ['./app/fonts/**/*'],
+    dest: './build/fonts'
   },
 
-  'sourceDir': './app/',
-  'buildDir': './build/',
+  sourceDir: './app/',
+  buildDir: './build/',
 
-  'api': {
-    'prod': 'http://api.monolist.co/v1/'
+  api: {
+    prod: 'http://api.monolist.co/v1/'
   }
 
 };
